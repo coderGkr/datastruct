0dbc4efa634de2fe51e2896fd3286c3b1c080a4d
Gkr_LenovoWIN <gopu7381@gmail.com>
2017-11-15 20:51:30 +0530

    fixed search

--- bst.py
+++ bst.py
@@ -28,27 +28,41 @@ class bst:
 			self.right.disptree(val+1)
 	def search(self,val):
 		if self.val==val:
-			return 1
+			return True
 		elif val <= self.val:
 			if self.left is None:
-				return 0
+				return False
 			else:
-				self.left.search(val)
+				return self.left.search(val)
 		else:
 			if self.right is None:
-				return 0
+				return False
 			else:
-				self.right.search(val)
+				return self.right.search(val)
+
+	def successor(val):
+		if self.search(val) is True:
+			if self.right is not None:
+				a=self.right
+				while a.left is not None:
+					a=a.left
+				return a.val
+		else:
+			return "No Such Value"
+
+#redifine search so as to return the position of the object, if not define something to get the position of the interger in the tree before finding successor of the item.
+
+
 
 #Inorder successor
 #inorder predecessor
 
 #delete a node
-
-a=[87, 50, 41, 53, 6, 4, 94, 72, 40, 17]
 # import random
 # for i in range(0,10):
 # 	a.append(random.randint(1,45))
+a=[87, 50, 41, 53, 6, 4, 94, 72, 40, 17]
+
 
 k=bst()
 for i in a:
@@ -58,7 +72,7 @@ for i in a:
 print k.search(65)
 
 print k.search(53)
-
+print k.successor(4)
 
 
 # a=raw_input("enter the numbr")
